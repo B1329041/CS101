86044775d66a35005b0d3d63cd14c096f1f42847
B1329041 <B1329041@cgu.edu.tw>
2024-12-05 15:09:17 +0800

    Update main1.c

--- EX3/main1.c
+++ EX3/main1.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
-
 int main() {
-    int i = 10; // 否
-    // int i = 8; // 是
-
-
+    int i= 8；
+    if (i & (i-1)) {
+        printf("false");
+    }
+    else {
+        printf("true");
+    }
     return 0;
 }
