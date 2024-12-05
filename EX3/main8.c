ebd14ea91663bcff247a76e070da74ea9ee7f6b5
B1329041 <B1329041@cgu.edu.tw>
2024-12-05 15:33:39 +0800

    Create main8.c

---
+++ EX3/main8.c
@@ -0,0 +1,14 @@
+#include ‹stdio.h>
+int main() {
+    int x = -9;
+    int y = 8;
+    int z = 2;
+    if (x < 0) {
+        int sum = (x*-1)*100 + y*10 + z;
+        printf("%d", sum*-1);
+    ｝
+    else {
+        printf("%d", x*100 + y*10 + z);
+    }
+    return 0;
+｝
